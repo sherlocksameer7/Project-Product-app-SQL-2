cf785becdee7195b4714e5f6865db3091b97f23c
sherlocksameer7 <arijitsameers@gmail.com>
2022-03-08 14:13:06 +0530

    Product app has been successfully implemented

---
+++ product_project.py
@@ -0,0 +1,25 @@
+import sqlite3
+
+product_details = sqlite3.connect("product.db")  # creating a database
+
+# product_details.execute('''   Create Table ProductData(
+#                       ProductCode Integer Primary Key Autoincrement,
+#                       ProductName Text,
+#                       ProductPrice Integer,
+#                       ProductDistributorName Text,
+#                       ManufacturerName Text
+# );     ''')
+# print("Table Created Successfully")
+
+get_product_name = input ("Enter the product name: ")
+get_product_price = input ("Enter the product price: ")
+get_distributer_name = input ("Enter the distributer name: ")
+get_manufacturer_name = input ("Enter the manufacturer name: ")
+
+product_details.execute("Insert Into ProductData(ProductName, ProductPrice, ProductDistributorName, ManufacturerName) \
+                        Values('"+get_product_name+"',"+get_product_price+",'"+get_distributer_name+"','"+get_manufacturer_name+"')")
+
+product_details.commit()
+product_details.close()
+
+print("Inserted successfully")
